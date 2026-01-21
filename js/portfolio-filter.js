4493e1bdbf98752764973534f690f049c8ec2d12
SwitzerEvans <chiemekaevans928@gmail.com>
2026-01-22 00:42:02 +0100

    Add files via upload

---
+++ js/portfolio-filter.js
@@ -0,0 +1,32 @@
+(() => {
+    const buttons = document.querySelectorAll('.portfolio-buttons button');
+    const portfolios = document.querySelectorAll('.portfolio');
+
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            const filter = button.dataset.filter;
+
+            portfolios.forEach(portfolio => {
+                const category = portfolio.dataset.category;
+            
+                if (filter === 'all' || category === filter) {
+                    // show immediately
+                    portfolio.style.display = 'grid';
+                    setTimeout(() => {
+                        portfolio.classList.remove('hide');
+                    }, 10); // tiny delay to allow transition
+                } else {
+                    // fade out first
+                    portfolio.classList.add('hide');
+            
+                    // then remove from grid after transition
+                    setTimeout(() => {
+                        portfolio.style.display = 'none';
+                    }, 400); // match transition duration
+                }
+            });
+            
+            
+        });
+    });
+})();
